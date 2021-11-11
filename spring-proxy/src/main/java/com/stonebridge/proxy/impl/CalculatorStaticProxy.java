2e45d4288a113fd88aac192803dca07843b0964e
stonebridge <stonebridge@njfu.edu.cn>
2021-11-11 21:11:16 +0800

    3.2.2.静态代理

---
+++ spring-proxy/src/main/java/com/stonebridge/proxy/impl/CalculatorStaticProxy.java
@@ -0,0 +1,37 @@
+package com.stonebridge.proxy.impl;
+
+import com.stonebridge.proxy.api.Calculate;
+
+public class CalculatorStaticProxy implements Calculate {
+    // 将被代理的目标对象声明为成员变量
+    private Calculate target;
+
+    public CalculatorStaticProxy(Calculate target) {
+        this.target = target;
+    }
+
+    @Override
+    public int add(int i, int j) {
+        // 附加功能由代理类中的代理方法来实现
+        System.out.println("[日志] add 方法开始了，参数是：" + i + "," + j);
+        // 通过目标对象来实现核心业务逻辑
+        int addResult = target.add(i, j);
+        System.out.println("[日志] add 方法结束了，结果是：" + addResult);
+        return addResult;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        return 0;
+    }
+}
