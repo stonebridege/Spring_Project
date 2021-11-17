9cee6bc01e94aa81fdbc856737e000fc3753e3d5
stonebridge <stonebridge@njfu.edu.cn>
2021-11-17 22:11:34 +0800

    5.2.2.2.添加事务功能

--- spring-tx/src/main/java/com/stonebridge/service/EmpService.java
+++ spring-tx/src/main/java/com/stonebridge/service/EmpService.java
@@ -3,6 +3,7 @@ package com.stonebridge.service;
 import com.stonebridge.dao.EmpDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmpService {
@@ -16,9 +17,11 @@ public class EmpService {
     // 为了便于核对数据库操作结果，不要修改同一条记录
     // 修改员工姓名的一组参数 (empId4EditName,newName)
     // 修改员工工资的一组参数 (empId4EditSalary,newSalary)
+    @Transactional
     public void updateTwice(Integer empId4EditName, String newName, Integer empId4EditSalary, Double newSalary) {
         // 为了测试事务是否生效，执行两个数据库操作，看它们是否会在某一个失败时一起回滚
         empDao.updateEmpNameById(empId4EditName, newName);
+        int a = 10 / 0;
         empDao.updateEmpSalaryById(empId4EditSalary, newSalary);
     }
 }
